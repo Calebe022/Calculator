32661768985bd4eebcb8b751a09712d27aeeeda1
Calebe022 <calebeandre946@gmail.com>
2026-03-17 13:58:07 -0300

    Refactor: ajustando main para padrão Java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
         int opcao = 0;
